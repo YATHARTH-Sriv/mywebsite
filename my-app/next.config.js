dde12fa73f6016ed4a3959d889d95e572e52c61f
yatharth srivastava <yatharthsriv18@gmail.com>
2024-04-11 13:01:38 +0530

    adding hostnames

---
+++ my-app/next.config.js
@@ -0,0 +1,8 @@
+const { hostname } = require("os");
+
+module.exports = {
+    images: {
+      domains: ['avatars.githubusercontent.com',"i.pinimg.com","encrypted-tbn0.gstatic.com","1000logos.net","w1.pngwing.com","e7.pngegg.com","seeklogo.com","git-scm.com","upload.wikimedia.org","static.vecteezy.com", "logowik.com"],
+      
+    },
+  };
